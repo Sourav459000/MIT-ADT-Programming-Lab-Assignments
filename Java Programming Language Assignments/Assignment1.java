5313c35fc8005cfe8b8cdacc268043fd80f5ea8d
Sourav Toshniwal <sourav280902@gmail.com>
2023-02-05 20:00:49 +0530

    5 February

--- Java Programming Language Assignments/Assignment1.java
+++ Java Programming Language Assignments/Assignment1.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Assignment1{
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.println("Enter four Integers:");
-            int firstNumber = scanner.nextInt();
-            int secondNumber = scanner.nextInt();
-            int thirdNumber = scanner.nextInt();
-            int fourthNumber = scanner.nextInt();
-            if ((firstNumber == secondNumber) && (secondNumber == thirdNumber) && (thirdNumber == fourthNumber)) {
-                System.out.println("All four Integers are equal.");
-            }
-            else{
-                System.out.print("All four Integers are not equal.");
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter four Integers:");
+        int firstNumber = scanner.nextInt();
+        int secondNumber = scanner.nextInt();
+        int thirdNumber = scanner.nextInt();
+        int fourthNumber = scanner.nextInt();
+        if ((firstNumber == secondNumber) && (secondNumber == thirdNumber) && (thirdNumber == fourthNumber)) {
+            System.out.println("All four Integers are equal.");
         }
+        else{
+            System.out.print("All four Integers are not equal.");
+        }
+        
     }
 }
