a362225415d37a1b649ec2d86b67bc2aa78c45f0
Sourav Toshniwal <sourav280902@gmail.com>
2023-02-09 21:38:26 +0530

    9 February

--- Java Programming Language Assignments/Assignment2.java
+++ Java Programming Language Assignments/Assignment2.java
@@ -1,21 +1,24 @@
 import java.util.Scanner;
 
-class Student2
-{
-    private String name, city;
-    private int age;
+class Student2 {
+    private String name;
+    private String rollno;
+    private int marks;
     Scanner scanner = new Scanner(System.in);
-
-    public void getData()
-    {
-        System.out.println("Enter the name, name of city and age of student:");
-        name = scanner.next();
-        city = scanner.next();
-        age = scanner.nextInt();
+    public void getData() {
+        System.out.println("Enter the Name, Roll No. and Marks of student: ");
+        String name = scanner.nextLine();
+        String rollno = scanner.nextLine();
+        int marks = scanner.nextInt();
+        this.name = name;
+        this.rollno = rollno;
+        this.marks = marks;
     }
-    public void printData()
-    {
-        System.out.println("Student name: " + name + ", Student city: " + city + ", Student age: " + age);
+
+    public void printData() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Roll number: " + this.rollno);
+        System.out.println("Marks: " + this.marks);
     }
 }
 public class Assignment2 {
@@ -23,8 +26,10 @@ public class Assignment2 {
         Student2 s1=new Student2();
         Student2 s2=new Student2();
         s1.getData();
-        s1.printData();
         s2.getData();
+        System.out.println("\nDetails of Student 1 are:");
+        s1.printData();
+        System.out.println("\nDetails of Student 2 are:");
         s2.printData();
     }
 }
